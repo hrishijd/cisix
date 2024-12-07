bdcd3fd48a860444205c42fb1b505d0d8abc401f
shashwat0309 <shashwat.mohite2001@gmail.com>
2024-12-07 22:45:28 +0530

    Completed the executionJS route

--- migrations/20241207052416-create-execution-code.js
+++ migrations/20241207052416-create-execution-code.js
@@ -1,46 +1,43 @@
-'use strict';
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('ExecutionCodes', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      executionId: {
-        type: Sequelize.INTEGER
-      },
-      userId: {
-        type: Sequelize.INTEGER
-      },
-      executionBlockNo: {
-        type: Sequelize.STRING
-      },
-      executionFileName: {
-        type: Sequelize.STRING,
-      },
-      executionAkaveID: {
-        type: Sequelize.STRING,
-      },
-      executionBlockState: {
-        type: Sequelize.STRING
-      },
-      executionChainIds: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('ExecutionCodes');
-  }
-};
+  'use strict';
+  /** @type {import('sequelize-cli').Migration} */
+  module.exports = {
+    async up(queryInterface, Sequelize) {
+      await queryInterface.createTable('ExecutionCodes', {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER
+        },
+        useremail: {
+          type: Sequelize.STRING
+        },
+        executionBlockNo: {
+          type: Sequelize.STRING
+        },
+        executionFileName: {
+          type: Sequelize.STRING,
+        },
+        executionAkaveID: {
+          type: Sequelize.STRING,
+        },
+        executionBlockState: {
+          type: Sequelize.STRING
+        },
+        executionChainIds: {
+          type: Sequelize.INTEGER
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        }
+      });
+    },
+    async down(queryInterface, Sequelize) {
+      await queryInterface.dropTable('ExecutionCodes');
+    }
+  };
