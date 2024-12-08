4f1625715256f3b537b0a3eb335df9a69e32e7fb
shashwat0309 <shashwat.mohite2001@gmail.com>
2024-12-08 06:06:21 +0530

    Fix user controller

--- controller/userCreate.js
+++ controller/userCreate.js
@@ -2,15 +2,15 @@ const User = require("../models/RegistrationData");
 
 const registerUser = async (req, res) => {
   try {
-    const { fullName, email, walletAddress, role, password } = req.body;
+    const { fwalletAddress } = req.body;
 
     // Validate the input data
-    if (!fullName || !email || !walletAddress || !role || !password) {
-      return res.status(400).send("All fields are required");
+    if (!walletAddress) {
+      return res.status(400).send("Wallet Address field is required");
     }
 
     // Check if the user already exists
-    const exist = await User.findOne({ where: { email } });
+    const exist = await User.findOne({ where: { walletAddress } });
     if (exist) {
       return res.status(409).send("User already exists");
     }
@@ -21,7 +21,11 @@ const registerUser = async (req, res) => {
     });
 
     // Return the created user (excluding the password)
-    return res.status(201).send("User Created");
+    return {
+      success: true,
+      message: "User Created Successfully",
+      data: user,
+    };
   } catch (error) {
     console.error("Error registering user:", error);
     return res.status(500).send("An error occurred while registering the user");
