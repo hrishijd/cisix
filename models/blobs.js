c34299681b43c7aebbab0d276a1a107d67981768
theR4actGuy <Vivek Jain>
2024-12-08 05:39:43 +0530

    model type fix

--- models/blobs.js
+++ models/blobs.js
@@ -1,29 +1,33 @@
-var Sequelize = require('sequelize');
-var sequelize = require('../../Express Database/connection');
+var Sequelize = require("sequelize");
+var sequelize = require("../../Express Database/connection");
 
-var blobs = sequelize.define('blobs', {
-  blobId: {
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
-    type: Sequelize.INTEGER,
+var blobs = sequelize.define(
+  "blobs",
+  {
+    blobId: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    walletAddress: {
+      type: Sequelize.STRING,
+    },
+    executionId: {
+      type: Sequelize.INTEGER,
+    },
+    BlockNo: {
+      type: Sequelize.STRING,
+    },
   },
-  walletAddress: {
-    type: Sequelize.INTEGER,
-  },
-  executionId: {
-    type: Sequelize.INTEGER,
-  },
-  BlockNo: {
-    type: Sequelize.STRING,
-  },
-}, {
-  createdAt: true,
-  updatedAt: true,
-  freezeTableName: true,
-  timestamps: true,
-  logging: false,
-});
+  {
+    createdAt: true,
+    updatedAt: true,
+    freezeTableName: true,
+    timestamps: true,
+    logging: false,
+  }
+);
 
 // sequelize.sync({ alter: true });
 module.exports = blobs;
